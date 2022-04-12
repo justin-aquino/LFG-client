813e4dd88241d802a1c8a48e6b0874d538459074
justin-aquino <justin.aquino25@gmail.com>
2022-04-12 14:17:34 -0700

    init commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { Navigate } from 'react-router-dom'
+
+export default function Login ({ currentUser, setCurrentUser }) {
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  const [message, setMessage] = useState('')
+//   console.log(form.email)
+
+  const handleFormSubmit = async e => {
+    // e.preventDefault()
+    // try {
+      
+    //   const response = await axios.post(
+    //     `${process.env.REACT_APP_SERVER_URL}/api-v1/users/login`,
+    //     form)
+        
+    
+    //   // decode the token that is sent to me
+    //   const { token } = response.data
+    //   const decoded = jwt_decode(token)
+    // //   console.log(decoded)
+
+    //   // save the token in localStorage
+    //   localStorage.setItem('jwt', token)
+
+    //   // sets the app state to the logged in user
+    //   setCurrentUser(decoded)
+
+    // } catch (err) {
+    //   console.log(err)
+    //   if(err.response.status === 409 || err.response.status === 406) {
+    //         // console.log(err.response.data)
+    //         setMessage('Invalid login credentials.')
+    //         // console.log(err.response.data.message)
+    //     }
+      
+    // //   console.log(message)
+    // }
+  }
+
+
+  if (currentUser) return <Navigate to='/dashboard' />
+
+  return (
+    <>    
+    <div className='flex-container'><h3>Login</h3></div>      
+    <div className='flex-container'>    
+        <div className='form-container'>        
+        <form onSubmit={handleFormSubmit}>
+          <p>
+            <label htmlFor='username'>Username:</label>         
+            <input
+              id='username'
+              type='text'
+              onChange={e => setForm({ ...form, username: e.target.value })}
+              value={form.username}
+              required
+            />
+          </p>
+          <p>
+            <label htmlFor='password'>Password:</label>          
+            <input
+              id='password'
+              type='password'
+              onChange={e => setForm({ ...form, password: e.target.value })}
+              value={form.password}
+              required
+            />
+          </p>
+          <button type='submit'>Submit</button>
+        </form>        
+        <p className='error-message'>{message ? `${message}` : ''}</p>
+      
+      </div>
+      </div>
+    </>
+  )
+}
