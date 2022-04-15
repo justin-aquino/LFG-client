8320fbd17f846b4d6befdd7219cde6c114318fae
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-04-14 20:31:41 -0700

    updates to profile page

--- src/components/pages/Users/Dashboard.jsx
+++ src/components/pages/Users/Dashboard.jsx
@@ -1,18 +1,37 @@
 import axios from 'axios';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Dashboard = ({ games, currentUser }) => {
+
+  const navigate = useNavigate()
+  const [msg, setMessage] = useState("")
   const [form, setForm] = useState({
+    id: currentUser.id,
+    game_fk: "",
+    username: "",
+    gameName: ""
+
+  });
+
+ useEffect(() => {
+   setForm({
     id: currentUser.id,
     game_fk: "",
     username: ""
 
-  });
-
+  })
+ }, [])
   const handleSelect =(e)=>{
-    console.log(e)
-    setForm({...form, game_fk : e.target.value})
+    console.log(games)
+    const game = games.find((game, idx)=>{
+        console.log(e.target.value)
+          return game._id === e.target.value
+        })
+        console.log(game)
+    setForm({...form, game_fk : e.target.value, gameName: game.name})
 
   }
   const allGames = games.map((element, idx) => {
@@ -33,16 +52,28 @@ const Dashboard = ({ games, currentUser }) => {
   //   )
   // })
   
-
   const handleEditSubmit = async (e) => {
     e.preventDefault();
+    setMessage("")
+    try{
 
       await axios.put(`${process.env.REACT_APP_SERVER_URL}/users/edit`, form)
       .then(response=>{
         console.log(response.data)
       })
-
+      setForm({
+        id: currentUser.id,
+        game_fk: "",
+        username: ""
+    
+      })
+      navigate("/")
+      navigate("/dashboard")
+    } catch(err){
+      setMessage(err.response.data.msg)
+    }
   };
+
 
   return (
     <>
@@ -51,15 +82,18 @@ const Dashboard = ({ games, currentUser }) => {
         <h3>Email: {currentUser.email}</h3>
         <h2>All Game Alias</h2>
         {/* {allAlias} */}
-        <form onSubmit={handleEditSubmit}>
+        
+        <form action="https://google.ca" onSubmit={handleEditSubmit}>
           {/* <input hidden name="userId" type="text">{currentUser.id}</input> */}
           <label htmlFor="select game">Add a game alias</label>
           <select name="games" id="games" onChange={handleSelect}>
+          <option disabled="disabled" selected="selected">Select an option.</option>
             {allGames}
           </select>
           <input type="text" value={form.username} onChange={e => setForm({ ...form, username: e.target.value })}></input>
           <input type="submit" />
         </form>
+        {msg ? <h2 style={{color: "red"}}>{msg}</h2> : <></>} 
       </div>
     </>
   );
